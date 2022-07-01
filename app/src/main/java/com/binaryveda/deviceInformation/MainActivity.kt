0b18fe044c0f9b6c817152e71287fbfc4e03225d
Shankar Kakumani <shankarchowdary7545@gmail.com>
2022-07-01 14:18:52 +0530

    added Permission

--- app/src/main/java/com/binaryveda/deviceInformation/MainActivity.kt
+++ app/src/main/java/com/binaryveda/deviceInformation/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.binaryveda.deviceInformation
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.binaryveda.deviceInformation.model.DeviceInformation
 import com.binaryveda.deviceInformation.utils.DeviceUtils
 import com.google.firebase.database.DatabaseReference
@@ -14,16 +19,28 @@ import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
+    private val permissionContract =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if(it) {
+                writeDataToFirebase()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = Firebase.database.reference
-        writeDataToFirebase(DeviceUtils().getDeviceInformation(this))
+        if(havePermission(Manifest.permission.READ_PHONE_STATE)) {
+            database = Firebase.database.reference
+            writeDataToFirebase()
+        } else {
+            permissionContract.launch(Manifest.permission.READ_PHONE_STATE)
+        }
+
     }
 
-    private fun writeDataToFirebase(deviceInformation: DeviceInformation) {
+    private fun writeDataToFirebase() {
+        val deviceInformation = DeviceUtils().getDeviceInformation(this)
         database.child("users").child(deviceInformation.deviceId).setValue(deviceInformation)
             .addOnFailureListener {
                 Toast.makeText(this, "Failed to update data in Firebase - ${it.message}", Toast.LENGTH_LONG).show()
@@ -31,5 +48,12 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Data written to Firebase", Toast.LENGTH_LONG).show()
         }
         findViewById<TextView>(R.id.textView).text = Gson().toJson(deviceInformation)
+    }
+
+    fun Context.havePermission(permission: String): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
     }
 }
